30734799db6710d65120f92fcf1f50419618f163
Duha Mamdouh <dohamamdouh123@gmail.com>
2023-12-19 18:15:10 +0200

    Enhance order invoice

---
+++ app/Exceptions/UnsupportedCheckoutMethodException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Common\Enums\Message;
+use App\Common\Response;
+use Exception;
+
+class UnsupportedCheckoutMethodException extends Exception
+{
+    /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        return false;
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return (new Response())->exception(Message::UNSUPPORTED_CHECKOUT_METHOD);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use App\Common\Enums\Message;
 use App\Common\Services\CheckoutService;
 use App\Common\Services\ServiceOnlyCheckout;
 use App\Common\Services\TaxAndServiceCheckout;
+use App\Exceptions\UnsupportedCheckoutMethodException;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,13 +18,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(CheckoutService::class, function () {
-            switch (request()->checkout_method) {
+            switch (request()->checkout_method ?? 'taxAndService') {
                 case CheckoutMethod::WITH_TAX_AND_SERVICE:
                     return new TaxAndServiceCheckout;
                 case CheckoutMethod::WITH_SERVICE_ONLY:
                     return new ServiceOnlyCheckout;
                 default:
-                    throw new \RuntimeException(Message::UNSUPPORTED_CHECKOUT_METHOD);
+                    throw new UnsupportedCheckoutMethodException();
             }
         });
     }
